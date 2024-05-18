7747f4ad32026aa5fe4bdb901ff2ab43a11825a5
omivrex <omivrex@gmail.com>
2024-05-18 10:20:52 +0100

    integrated open ai loibrary

--- src/configs/config.ts
+++ src/configs/config.ts
@@ -1,8 +1,13 @@
 import dotenv from "dotenv";
+import OpenAI from "openai";
 
 dotenv.config();
 
+const openAi = new OpenAI({
+    apiKey: process.env.OPENAI_API_KEY,
+});
+
 export const config = {
     port: process.env.PORT || 3000,
-    openaiApiKey: process.env.OPENAI_API_KEY || "",
+    openAi: openAi,
 };
